32994f5684e72346e1a8cc05a95c4652f5f99253
luthfi <luthfiandini1909@gmail.com>
2025-05-30 14:03:21 +0700

    add shop

---
+++ src/pages/AboutPages.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../Components/Navbar';
+import HeroSection from '../Components/Hero-Section';
+import Footer from '../Components/Footer';
+
+function AboutPages() {
+  return (
+    <div>
+      <Navbar />
+      <HeroSection />
+      <Footer />
+    </div>
+  );
+}
+
+export default AboutPages
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../Components/Navbar";
+import HeroSection from "../Components/Hero-Section";
+
+
+export default function HomePage() {
+  return (
+    <div>
+      <Navbar/>
+      <HeroSection />
+      
+    </div>
+  );
+}

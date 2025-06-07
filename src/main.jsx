0c38a720961a413c30db140016f05473d1c35ba1
luthfi <luthfiandini1909@gmail.com>
2025-06-07 10:32:53 +0700

    last

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {BrowserRouter, Route, Routes} from 'react-router'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import KontakSection from './Components/Kontak-Section'
 import AboutPages from './pages/AboutPages'

e3ee259ddcdaafdb96f9e5d857556d88fbe2e427
luthfi <luthfiandini1909@gmail.com>
2025-06-07 10:14:42 +0700

    dom lagi

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
 import HeroSection from "../Components/Hero-Section";
-import {Routes, Route} from 'react-router';
+import {Routes, Route} from 'react-router-dom';
 import ShopPage from "../Components/ShopPage";
 import KontakSection from "../Components/Kontak-Section";
 
